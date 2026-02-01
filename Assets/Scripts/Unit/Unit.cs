261e07641fe8b64fe1ff4230c25f12d5b327341b
jabgok <lao2000@naver.com>
2026-02-02 02:32:28 +0900

    [Update] 유닛 초기 함수 설정
    1. 스탯
    2. 상태
    3. 이동

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -1,16 +1,70 @@
 using UnityEngine;
 
+public enum UnitState
+{
+    Move,
+    Attack,
+    Die,
+}
+
+/// <summary>
+/// Unit의 최상위 클래스
+/// <para>기본적인 스탯, 로직 포함</para>
+/// </summary>
 public class Unit : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("스탯")]
+    [SerializeField] private float maxHp;
+    [SerializeField] private float currentHp;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float bassMoveSpeed;
+    [SerializeField] private float speedModifier;
+
+    [Header("현재 상태")]
+    [SerializeField] public UnitState currentState; 
+
+    //프로퍼티
+    public float CurrentHp => currentHp;
+    public float MoveSpeed => moveSpeed;
+
+    protected virtual void Start()
     {
-        
+        Init();
+        currentState = UnitState.Move;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Init()
     {
-        
+        currentHp = maxHp;
+        moveSpeed = bassMoveSpeed;
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case UnitState.Move:
+                MoveState();
+                break;
+            case UnitState.Attack:
+                AttackState();
+                break;
+            case UnitState.Die:
+                DieState();
+                break;
+        }
+    }
+
+    protected virtual void MoveState()
+    {
+        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+    }
+    protected virtual void AttackState()
+    {
+
+    }
+    protected virtual void DieState()
+    {
+
     }
 }
