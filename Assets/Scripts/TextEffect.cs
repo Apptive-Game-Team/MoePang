7be8d5449f310798393f85d80f2aef3d392dbcdb
jabgok <127014921+Jinwook700@users.noreply.github.com>
2026-02-16 16:30:32 +0900

    [Feat] TextEffect : 타이틀 글자 깜빡거림 효과

---
+++ Assets/Scripts/TextEffect.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// 글자 효과
+/// </summary>
+public class TextEffect : MonoBehaviour
+{
+    [Header("타이틀 텍스트")]
+    [SerializeField] private TextMeshProUGUI textComponent;
+    [SerializeField] private float blinkSpeed = 2.0f; // 깜빡임 속도
+
+    void Update()
+    {
+        float alpha = (Mathf.Sin(Time.time * blinkSpeed) + 1.0f) * 0.5f;
+
+        Color color = textComponent.color;
+        color.a = alpha;
+        textComponent.color = color;
+    }
+}
