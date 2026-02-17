090b0660ce7d7a186fae601265953690722bb2fd
jabgok <lao2000@naver.com>
2026-02-18 00:14:03 +0900

    [Feat] Title -> Main 씬 전환 구현

--- Assets/Scripts/TitleSceneEvent.cs
+++ Assets/Scripts/TitleSceneEvent.cs
@@ -1,11 +1,18 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// 타이틀 씬 이벤트 담당 스크립트
+/// </summary>
 public class TitleSceneEvent : MonoBehaviour
 {
     [Header("타이틀 텍스트")]
     [SerializeField] private TextMeshProUGUI textComponent;
     [SerializeField] private float blinkSpeed = 2.0f; // 깜빡임 속도
+
+    [Header("이동할 씬")]
+    [SerializeField] private string nextSceneName;
 
     void Update()
     {
@@ -14,5 +21,15 @@ public class TitleSceneEvent : MonoBehaviour
         Color color = textComponent.color;
         color.a = alpha;
         textComponent.color = color;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToNextScene();
+        }
+    }
+
+    private void MoveToNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
