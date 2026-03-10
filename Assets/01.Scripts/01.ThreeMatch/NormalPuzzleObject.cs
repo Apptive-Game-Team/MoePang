9e254e2dcb2405b87740e16e592777a616d27e70
191012 <kimhj191012@naver.com>
2026-03-10 15:36:09 +0900

    update : Swap 기준 변경(MouseUp -> Drag이동), 특수타일 발동 조건 변경(더블클릭 -> 클릭)

--- Assets/01.Scripts/01.ThreeMatch/NormalPuzzleObject.cs
+++ Assets/01.Scripts/01.ThreeMatch/NormalPuzzleObject.cs
@@ -8,30 +8,54 @@ namespace _01.Scripts._01.ThreeMatch
         
         private Vector2 _firstTouchPos;
         private Vector2 _lastTouchPos;
+        private bool _isSwapped;
 
         public override int GetPuzzleSubType() => (int)normalPuzzleType;
         
         private void OnMouseDown()
         {
             _firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            _isSwapped = false;
         }
-
-        private void OnMouseUp()
+        
+        private void OnMouseDrag()
         {
+            if (_isSwapped)
+            {
+                return;
+            }
+
             _lastTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            CalculateSwipe();
+            float distance = Vector2.Distance(_firstTouchPos, _lastTouchPos);
+            
+            if (distance > 0.5f) 
+            {
+                CalculateSwap();
+                _isSwapped = true;
+            }
         }
 
-        private void CalculateSwipe()
+        private void CalculateSwap()
         {
             float swipeAngle = Mathf.Atan2(_lastTouchPos.y - _firstTouchPos.y, _lastTouchPos.x - _firstTouchPos.x) * Mathf.Rad2Deg;
             
             if (Vector2.Distance(_firstTouchPos, _lastTouchPos) < 0.1f) return;
 
-            if (swipeAngle > -45 && swipeAngle <= 45) Swap(1, 0);
-            else if (swipeAngle > 45 && swipeAngle <= 135) Swap(0, 1);
-            else if (swipeAngle > 135 || swipeAngle <= -135) Swap(-1, 0);
-            else if (swipeAngle < -45 && swipeAngle >= -135) Swap(0, -1);
+            switch (swipeAngle)
+            {
+                case > -45 and <= 45:
+                    Swap(1, 0);
+                    break;
+                case > 45 and <= 135:
+                    Swap(0, 1);
+                    break;
+                case > 135 or <= -135:
+                    Swap(-1, 0);
+                    break;
+                case < -45 and >= -135:
+                    Swap(0, -1);
+                    break;
+            }
         }
 
         private void Swap(int dirX, int dirY)
