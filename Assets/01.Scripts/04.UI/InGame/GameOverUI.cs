74cb48f41d5121dbb4af516f77a8b0111a002343
Gimlocal <127363458+Gimlocal@users.noreply.github.com>
2026-04-15 23:10:09 +0900

    feat : 게임 오버/클리어 패널 조정

--- Assets/01.Scripts/04.UI/InGame/GameOverUI.cs
+++ Assets/01.Scripts/04.UI/InGame/GameOverUI.cs
@@ -5,19 +5,10 @@ namespace _01.Scripts._04.UI.InGame
 {
     public class GameOverUI : GameUI
     {
-        [SerializeField] private TextMeshProUGUI timeText;
-        [SerializeField] private TextMeshProUGUI coinText;
-        
         protected override void OnEnable()
         {
             StageManager.Instance.StopStage();
-            float time = StageManager.Instance.CurrentTime;
-            int minutes = Mathf.FloorToInt(time / 60);
-            int seconds = Mathf.FloorToInt(time % 60);
-            
-            Time.timeScale = 0.1f;
-            timeText.text = $"{minutes}:{seconds:00}";
-            coinText.text = $"{GoldManager.Instance.Gold}";
+            Time.timeScale = 0;
         }
     }
 }
