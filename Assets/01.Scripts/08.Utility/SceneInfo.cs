5e0555eeccb6295fcb4996d8fc58ad85f9f4e52c
191012 <kimhj191012@naver.com>
2026-04-22 16:22:49 +0900

    feat : 씬 이름 설정 추가

---
+++ Assets/01.Scripts/08.Utility/SceneInfo.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace _01.Scripts._08.Utility
+{
+    public enum SceneType
+    {
+        Title,
+        Main,
+        Shop,
+        MatchAndBattle,
+    }
+    
+    public static class SceneInfo
+    {
+        private static readonly Dictionary<SceneType, string> SceneNames = new()
+        {
+            { SceneType.Title , "00.TitleScene"},
+            { SceneType.Main, "01.MainScene"},
+            { SceneType.Shop, "02.ShopScene"},
+            { SceneType.MatchAndBattle, "03.3match&Battle"},
+        };
+
+        public static string GetSceneName(SceneType type)
+        {
+            return SceneNames[type];
+        }
+    }
+}
